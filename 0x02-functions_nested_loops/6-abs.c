5b32b56a297522ff8fc99b6565bee23522659feb
Oladimeji14 <demix1495@gmail.com>
2023-10-10 13:56:36 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * _abs - Inserts the entire value of an integer.
+ *  _abs - Inserts the entire value of an integer.
  * @c: The number to be inserted.
  * Return: Entire value of number or zero
- */ 
- 
+ */
+
 int _abs(int c)
 
 {
+
 	if (c < 0)
 	{
 	int abs_val;
