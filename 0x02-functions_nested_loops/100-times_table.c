0a41216042f03f1911a3f2d0c87e484edd2b22bb
Oladimeji14 <demix1495@gmail.com>
2023-10-11 13:30:23 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,40 +1,45 @@
 #include "main.h"
 
 /**
- * print_times_table - Print the multiplication table of the input, starting with 0
+ * print_times_table - Print the multiplication table of the input,
+ * starting with 0
  * @n: int type number
  */
 
 void print_times_table(int n)
-
 {
-
-	int num, qad, bab;
+	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
 	{
-	for (num = 0; num <= n; num++)
-	{
-	_putchar('0');
-	for (qad = 1; qad <= n; qad++)
-	{
-	_putchar(',');
-	_putchar(' ');
-	bab = num * qad;
-	if (bab <= 9)
-	_putchar(' ');
-	if (bab >= 100)
-	{
-	_putchar((bab / 100) + '0');
-	_putchar(((bab / 10)) % 10 + '0');
-	}
-	else if (bab <= 99 && bab >= 10)
-	{
-	_putchar((bab / 10) + '0');
-	}
-	_putchar((bab % 10) + '0');
-	}
-	_putchar('\n');
-	}
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
 	}
 }
