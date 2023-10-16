4fad0f0f1d96ae524e4f2bdee0bd665462bfdf37
Oladimeji14 <demix1495@gmail.com>
2023-10-16 13:35:51 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,32 +3,24 @@
 /**
   * rev_string - The function that print  reverses a string
   * @s: The string to be adjusted
-  *
-  * Return: void
+  * Return: String in reverses
   */
 void rev_string(char *s)
 {
-	int x, y, z;
-	char *a, aux;
+	char rev = s[0];
+	int magic = 0;
+	int u;
 
-	a = s;
-
-	while (s[y] != '\0')
+	while (s[magic] != '\0')
+	magic++;
+	for (u = 0; u < magic; u++)
 	{
-		y++;
-	}
-
-	for (z = 1; z < y; z++)
-	{
-		a++;
-	}
-
-	for (x = 0; x < (y / 2); x++)
-	{
-		aux = s[x];
-		s[x] = *a;
-		*a = aux;
-		a--;
+		magic--;
+		rev = s[u];
+		s[u] = s[magic];
+		s[magic] = rev;
 	}
 }
+	
+
 
