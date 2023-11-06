a7bbdfb97fc4588bb57f0b81a3cbf29178f69f46
Oladimeji14 <demix1495@gmail.com>
2023-11-05 23:52:12 -0800

    main.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+# 0x0E. C - Structures, typedef REAME.md
+My README.md file for the project 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * struct dog - Dog description
+  * @name: name given to the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The structure of a dog.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
