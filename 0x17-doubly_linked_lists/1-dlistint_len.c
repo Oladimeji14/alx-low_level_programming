e4bd86c08c9bfa9bb1b113f5724898e83d610565
Oladimeji14 <demix1495@gmail.com>
2024-01-11 09:25:32 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Counts a linked dlistint_t list.
- * @h: The head 
+ * dlistint_len - Counts nodes in a linked dlistint_t list.
+ * @h: The head of the list.
  *
- * Return: 0.
+ * Return: Number of nodes.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t nodes = 0;
+    size_t nodes = 0;
 
-	while (h)
-	{
-		nodes++;
-		h = h->next;
-	}
+    while (h)
+    {
+        nodes++;
+        h = h->next;
+    }
 
-	return (nodes);
+    return (nodes);
 }
